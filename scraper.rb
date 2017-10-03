302444066993625ada0d5d839e3c30e674a3522c
Dane Laban <TheZepto@users.noreply.github.com>
2017-10-03 16:49:38 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -3,24 +3,23 @@
 
 require 'scraperwiki'
 require 'mechanize'
+require 'date'
 
-# Scraping from Masterview 2.0
-
-def scrape_page(page)
-  page.at("table#ctl00_cphContent_ctl01_ctl00_RadGrid1_ctl00 tbody").search("tr").each do |tr|
-    tds = tr.search('td').map{|t| t.inner_text.gsub("\r\n", "").strip}
-    day, month, year = tds[3].split("/").map{|s| s.to_i}
+def scrape_page(page, comment_url)
+  page.at("table.rgMasterTable").search("tr.rgRow,tr.rgAltRow").each do |tr|
+    tds = tr.search('td').map{|t| t.inner_html.gsub("\r\n", "").strip}
+    day, month, year = tds[2].split("/").map{|s| s.to_i}
     record = {
       "info_url" => (page.uri + tr.search('td').at('a')["href"]).to_s,
-      "council_reference" => tds[1].split(" - ")[0].squeeze(" ").strip,
+      "council_reference" => tds[1],
       "date_received" => Date.new(year, month, day).to_s,
-      "description" => tds[1].split(" - ")[1..-1].join(" - ").squeeze(" ").strip,
-      "address" => tds[2].squeeze(" ").strip,
+      "description" => tds[3].gsub("&amp;", "&").split("<br>")[1].to_s.squeeze(" ").strip,
+      "address" => tds[3].gsub("&amp;", "&").split("<br>")[0].gsub("\r", " ").gsub("<strong>","").gsub("</strong>","").squeeze(" ").strip,
       "date_scraped" => Date.today.to_s
     }
-    record["comment_url"] = "https://sde.brisbane.qld.gov.au/services/startDASubmission.do?direct=true&daNumber=" + CGI.escape(record["council_reference"]) + "&sdeprop=" + CGI.escape(record["address"])
-    #p record
     if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+      puts "Saving record " + record['council_reference'] + " - " + record['address']
+#      puts record
       ScraperWiki.save_sqlite(['council_reference'], record)
     else
       puts "Skipping already saved record " + record['council_reference']
@@ -28,24 +27,21 @@ def scrape_page(page)
   end
 end
 
-def scrape_and_follow_next_link(page)
-  scrape_page(page)
-  nextButton = page.at('.rgPageNext')
-  puts "No further pages" if nextButton.nil?
-  unless nextButton.nil? || nextButton['onclick'] =~ /return false/
-    form = page.forms.first
-
-    # The joy of dealing with ASP.NET
-    form['__EVENTTARGET'] = nextButton['name']
-    form['__EVENTARGUMENT'] = ''
-    # It doesn't seem to work without these stupid values being set.
-    # Would be good to figure out where precisely in the javascript these values are coming from.
-    form['ctl00%24RadScriptManager1']=
-    'ctl00%24cphContent%24ctl00%24ctl00%24cphContent%24ctl00%24Radajaxpanel2Panel%7Cctl00%24cphContent%24ctl00%24ctl00%24RadGrid1%24ctl00%24ctl03%24ctl01%24ctl10'
-    form['ctl00_RadScriptManager1_HiddenField']=
-    '%3B%3BSystem.Web.Extensions%2C%20Version%3D3.5.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3D31bf3856ad364e35%3Aen-US%3A0d787d5c-3903-4814-ad72-296cea810318%3Aea597d4b%3Ab25378d2%3BTelerik.Web.UI%2C%20Version%3D2009.1.527.35%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3D121fae78165ba3d4%3Aen-US%3A1e3fef00-f492-4ed8-96ce-6371bc241e1c%3A16e4e7cd%3Af7645509%3A24ee1bba%3Ae330518b%3A1e771326%3Ac8618e41%3A4cacbc31%3A8e6f0d33%3Aed16cbdc%3A58366029%3Aaa288e2d'
-    page = form.submit(form.button_with(:name => nextButton['name']))
-    scrape_and_follow_next_link(page)
+# Implement a click on a link that understands stupid asp.net doPostBack
+def click(page, doc)
+  js = doc["href"] || doc["onclick"]
+  if js =~ /javascript:__doPostBack\('(.*)','(.*)'\)/
+    event_target = $1
+    event_argument = $2
+    form = page.form_with(id: "aspnetForm")
+    form["__EVENTTARGET"] = event_target
+    form["__EVENTARGUMENT"] = event_argument
+    form.submit
+  elsif js =~ /return false;__doPostBack\('(.*)','(.*)'\)/
+    nil
+  else
+    # TODO Just follow the link likes it's a normal link
+    raise
   end
 end
 
@@ -61,17 +57,28 @@ periodstrs.each {|periodstr|
   puts "Getting data in `" + periodstr + "`."
 
   url = "https://pdonline.brisbane.qld.gov.au/MasterViewUI/Modules/ApplicationMaster/default.aspx?page=found" + period + "&4a=&6=F"
+  comment_url = "mailto:council@logan.qld.gov.au"
 
   agent = Mechanize.new
+
   # Read in a page
   page = agent.get(url)
 
-  # This is weird. There are two forms with the Agree / Disagree buttons. One of them
-  # works the other one doesn't. Go figure.
   form = page.forms.first
   button = form.button_with(value: "I Agree")
-  raise "Can't find agree button" if button.nil?
-  page = form.submit(button)
+  form.submit(button)
+  # It doesn't even redirect to the correct place. Ugh
   page = agent.get(url)
 
-  scrape_and_follow_next_link(page)}
+  current_page_no = 1
+  next_page_link = true
+
+  while next_page_link
+    puts "Scraping page #{current_page_no}..."
+    scrape_page(page, comment_url)
+
+    current_page_no += 1
+    next_page_link = page.at(".rgPageNext")
+    page = click(page, next_page_link)
+    next_page_link = nil if page.nil?
+  end}
